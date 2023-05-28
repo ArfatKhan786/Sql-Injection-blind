3173f2b9b0c4853f06de79f03e2acbed4b52154e
Arfat Khan <109914040+ArfatKhan786@users.noreply.github.com>
2023-05-28 18:41:08 +0530

    Update Sqlblind.php

--- Sqlblind.php
+++ Sqlblind.php
@@ -4,10 +4,9 @@ echo "Hello, World!";  // Outputting a simple string
 
 echo "<h1>Welcome to my website</h1>";  // Outputting HTML content
 
-$name = "John Doe";
-$age = 25;
 
-echo "My name is " . $name . " and I am " . $age . " years old.";  // Outputting concatenated strings
+
+
 
 ?>
 
